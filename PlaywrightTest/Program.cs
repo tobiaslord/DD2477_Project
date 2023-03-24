67e6437cc688e23f13c20468546b03cc69fe4c90
Tobias Lord <tobias.lord@softone.se>
2023-03-24 12:47:46 +0100

    med exempel

--- PlaywrightTest/Program.cs
+++ PlaywrightTest/Program.cs
@@ -8,9 +8,11 @@ Console.WriteLine("Hello, World!");
 
 
 
-void Creep()
+using var playwright = await Playwright.CreateAsync();
+await using var browser = await playwright.Chromium.LaunchAsync();
+var page = await browser.NewPageAsync();
+await page.GotoAsync("https://playwright.dev/dotnet");
+await page.ScreenshotAsync(new()
 {
-    Playwright.CreateAsync();
-    
-
-}
+    Path = "screenshot.png"
+});
