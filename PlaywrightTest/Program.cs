816b72269d840babdaf83e21ac42252fba6daa8b
Felix Qvarfordt <felixq@kth.se>
2023-03-24 12:40:02 +0100

    removed binaries

--- PlaywrightTest/Program.cs
+++ PlaywrightTest/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
+
+using Microsoft.Playwright;
+
+// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+
+
+
+void Creep()
+{
+    Playwright.CreateAsync();
+    
+
+}
