8f1aaf89376295f4c5ee99134a5f8d3d885ed55b
chickenthug <viggo.svardkrona@gmail.com>
2023-05-02 14:28:46 +0200

    user book similarity

--- PlaywrightTest/Program.cs
+++ PlaywrightTest/Program.cs
@@ -9,7 +9,15 @@ class Program
     static async Task Main(string[] args)
     {
         Elastic es = new Elastic();
-        es.BetterSearch("romance");
+        User user = es.GetDocument<User>("4482859", "users");
+        Dictionary<string, double> user_vec = es.GetUserVector(user);
+        List<SimpleBook> books = es.BetterSearch("romance");
+        foreach (SimpleBook book in books)
+        {
+            Dictionary<string, double> book_vec = es.GetBookVector(book.genres, 0.7);
+            double sim = es.GetSimilarity(book_vec, user_vec);
+            Console.WriteLine("Title: {0} ------ Similarity: {1}", book.title, sim);
+        }
 
         //var cosmos = new CosmosScripts();
         //await cosmos.PerformRemoveDuplicates();
