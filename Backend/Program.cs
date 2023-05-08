e4cac5d72e38b707186d20bb1d2a02b4c0607fdd
Felix Qvarfordt <felix.qvarfordt@gmail.com>
2023-05-08 20:55:30 +0200

    Fixed program

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -9,9 +9,19 @@ class Program
 {
     static async Task Main(string[] args)
     {
+
+        // Set up an instance of Elastic Index
         ElasticIndex es = new ElasticIndex();
 
+        // Index all books from the json file
         es.IndexAllBooks();
+
+        // Remove duplicates from the index
+        es.CleanDatabase();
+
+        // Remove books in another language than english
+        es.CleanDatabaseEnglish();
+
         //es.CleanDatabase();
         //es.CleanDatabaseEnglish();
         //es.BetterSearch("romance");
